0475e1c5756ed153168e949a8991cc26421dd879
Ayeman <ayemanbsalauddin@gmail.com>
2022-08-17 07:02:03 +0600

    Reduced 'first load JS shared by all' from 188kb to 152kb

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPreact = require("next-plugin-preact");
+
+module.exports = withPreact({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
   },
-};
-
-module.exports = nextConfig;
+});
